a7229356eddb00e79007308fdc672d7d8b190016
Chaithra S <chaithragowry@gmail.com>
2022-12-07 10:45:50 +0530

    counter

--- src/App.js
+++ src/App.js
@@ -1,22 +1,68 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  
+  const [counter,setCounter]=useState(0);
+  function incrementCounter(){
+    setCounter(counter+1);
+  }
+  function decrementCounter(){
+    if(counter!=0){
+    setCounter(counter-1);
+    }
+  }
+  function resetCounter(){
+    setCounter(0);
+  }
+  return(
+    <div 
+    className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="counter">
+      <h3 style={{
+        color:'#57655E',
+        fontSize:25,
+      }}>Counter App</h3>
+      <h2 style={{
+        color:'#5B4285',
+        fontSize:35,
+        marginTop:'1px'
+      }}>{counter}</h2> 
+      <div className="align">
+      <button className="Button" style={{
+            margin:'15px',
+            padding:'1.5em',
+            borderRadius:'50%',
+            shadowRadius:3,
+            border:'1px solid #ededed',
+            fontWeight:'bold',
+            color:'#76A4DA',
+            
+            
+      }}
+              onClick={incrementCounter}>Add</button>
+      <button className="Button" style={{
+            margin:'15px',
+            padding:'1.5em',
+            borderRadius:'50%',
+            shadowRadius:3,
+            border:'1px solid #ededed',
+            fontWeight:'bold',
+            color:'#76A4DA'
+      }}
+        onClick={decrementCounter}>Minus</button>
+      <button className="Button" style={{
+            margin:'15px',
+            padding:'1.5em',
+            borderRadius:'50%',
+            shadowRadius:3,
+            border:'1px solid #ededed',
+            fontWeight:'bold',
+            color:'#98CBCC'
+      }}
+        onClick={resetCounter}>Reset</button></div>
+        </div>
       </header>
     </div>
   );
